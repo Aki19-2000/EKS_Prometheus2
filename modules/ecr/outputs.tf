144c5668354515f3e56d7e89962699fcf7560e90
pavithrasbidare <95730383+pavithrasbidare@users.noreply.github.com>
2025-03-09 03:53:16 +0530

    Update outputs.tf

--- modules/ecr/outputs.tf
+++ modules/ecr/outputs.tf
@@ -1,9 +1,1 @@
-output "patient_service_repository_url" {
-  description = "The URL of the patient service repository"
-  value       = aws_ecr_repository.patient_service.repository_url
-}
 
-output "appointment_service_repository_url" {
-  description = "The URL of the appointment service repository"
-  value       = aws_ecr_repository.appointment_service.repository_url
-}
