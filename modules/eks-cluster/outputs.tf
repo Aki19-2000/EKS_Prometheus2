25c57d734a5c6f7b0a5afe185a648076a5562d8f
pavithrasbidare <95730383+pavithrasbidare@users.noreply.github.com>
2025-03-08 21:59:56 +0530

    Update outputs.tf

--- modules/eks-cluster/outputs.tf
+++ modules/eks-cluster/outputs.tf
@@ -1,11 +1,11 @@
 output "cluster_id" {
   description = "The ID of the EKS cluster"
-  value       = aws_eks_cluster.eks_cluster.id
+  value       = aws_eks_cluster.eks.id
 }
 
 output "cluster_endpoint" {
   description = "The endpoint of the EKS cluster"
-  value       = aws_eks_cluster.eks_cluster.endpoint
+  value       = aws_eks_cluster.eks.endpoint
 }
 
 output "cluster_security_group_id" {
