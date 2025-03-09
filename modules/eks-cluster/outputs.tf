299bc9ae29b66eb6f9dcd9031e6100191a44a5b2
pavithrasbidare <95730383+pavithrasbidare@users.noreply.github.com>
2025-03-09 19:42:06 +0530

    Update outputs.tf

--- modules/eks-cluster/outputs.tf
+++ modules/eks-cluster/outputs.tf
@@ -1,22 +1,54 @@
-output "cluster_id" {
-  description = "The ID of the EKS cluster"
+# ------------------------------
+# EKS Cluster Outputs
+# ------------------------------
+output "eks_cluster_id" {
+  description = "EKS cluster ID"
   value       = aws_eks_cluster.eks.id
 }
 
-output "cluster_endpoint" {
-  description = "The endpoint of the EKS cluster"
+output "cluster_name" {
+  value = aws_eks_cluster.eks.name
+}
+
+
+output "eks_cluster_arn" {
+  description = "EKS cluster ARN"
+  value       = aws_eks_cluster.eks.arn
+}
+
+output "eks_cluster_endpoint" {
+  description = "EKS cluster API server endpoint"
   value       = aws_eks_cluster.eks.endpoint
 }
 
-output "cluster_security_group_id" {
-  description = "The security group ID of the EKS cluster"
-  value       = aws_security_group.eks_cluster_sg.id
+output "eks_cluster_ca_cert" {
+  description = "EKS cluster certificate authority"
+  value       = aws_eks_cluster.eks.certificate_authority[0].data
 }
 
-output "grafana_url" {
-  value = helm_release.prometheus.status[0].load_balancer[0].ingress[0].hostname
+output "cluster_endpoint" {
+  value = aws_eks_cluster.eks.endpoint
 }
 
-output "prometheus_url" {
-  value = helm_release.prometheus.status[0].load_balancer[0].ingress[0].hostname
+output "cluster_certificate_authority_data" {
+  value = aws_eks_cluster.eks.certificate_authority[0].data
+}
+
+
+# ------------------------------
+# Node Group Outputs
+# ------------------------------
+output "eks_node_group_id" {
+  description = "EKS node group ID"
+  value       = aws_eks_node_group.eks_nodes.id
+}
+
+output "eks_node_group_arn" {
+  description = "EKS node group ARN"
+  value       = aws_eks_node_group.eks_nodes.arn
+}
+
+output "eks_node_group_status" {
+  description = "Current status of the EKS node group"
+  value       = aws_eks_node_group.eks_nodes.status
 }
