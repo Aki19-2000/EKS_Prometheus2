131acc425f7c478e9ca739f64394dc4783bca330
pavithrasbidare <95730383+pavithrasbidare@users.noreply.github.com>
2025-03-09 10:26:04 +0530

    Update variables.tf

--- infra/variables.tf
+++ infra/variables.tf
@@ -17,13 +17,13 @@ variable "azs" {
 variable "public_subnets" {
   description = "The CIDR blocks for the public subnets"
   type        = list(string)
-  #default     = ["10.0.1.0/24", "10.0.2.0/24"]
+  default     = ["10.0.1.0/24", "10.0.2.0/24"]
 }
 
 variable "private_subnets" {
   description = "The CIDR blocks for the private subnets"
   type        = list(string)
-  #default     = ["10.0.3.0/24", "10.0.4.0/24"]
+  default     = ["10.0.3.0/24", "10.0.4.0/24"]
 }
 
 variable "cluster_name" {
