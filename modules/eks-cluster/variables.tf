fbbf2786d57fa192ae0125ed61a24ad449d1e7bb
pavithrasbidare <95730383+pavithrasbidare@users.noreply.github.com>
2025-03-09 11:07:52 +0530

    Update variables.tf

--- modules/eks-cluster/variables.tf
+++ modules/eks-cluster/variables.tf
@@ -2,9 +2,9 @@ variable "cluster_name" {
   description = "The name of the EKS cluster"
 }
 
-variable "cluster_version" {
+/*variable "cluster_version" {
   description = "The version of the EKS cluster"
-}
+}*/
 
 /*variable "subnets" {
   description = "The subnets for the EKS cluster"
